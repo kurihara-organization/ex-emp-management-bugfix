f8eaba4dda234048a932f088b0b7097990fc9297
0114Koki <koki.kurihara.0114@gmail.com>
2025-06-06 16:15:10 +0900

    (2-2) åã初級 入力値エラー処理 #2

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,17 +1,31 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
  * @author igamasayuki
  * 
  */
+
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message = "名前は必須です")
+	@Size(max = 50, message = "名前は50文字以内で入力してください")
 	private String name;
+
 	/** メールアドレス */
+	@NotBlank(message = "メールアドレスは必須です")
+	@Email(message = "メールアドレスの形式が正しくありません")
+	@Size(max = 100, message = "メールアドレスは100文字以内で入力してください")
 	private String mailAddress;
+
 	/** パスワード */
+	@NotBlank(message = "パスワードは必須です")
+	@Size(min = 8, max = 20, message = "パスワードは8〜20文字で入力してください")
 	private String password;
 
 	public String getName() {
